630a9e458630cbef59d59069faddad1325e10a5d
vaibhav-kumar07 <vaibhavtezan@gmail.com>
2026-02-22 12:41:43 +0530

    toploader added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@/app/components/shared/Header'
 import AppInitializer from '@/app/components/shared/AppInitializer'
 import { Suspense } from 'react'
 import { Toaster } from '@/components/ui/sonner'
+import NextTopLoader from 'nextjs-toploader'
 
 export const metadata: Metadata = {
   title: 'Bhandara Donation System',
@@ -19,6 +20,17 @@ export default async function RootLayout({
   return (
     <html lang="en" className="hide-scrollbar">
       <body className="hide-scrollbar">
+        <NextTopLoader
+          color="#f97316"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #f97316,0 0 5px #f97316"
+        />
         <AppInitializer />
         <div className="w-full h-screen overflow-hidden hide-scrollbar">
           <Suspense fallback={<div>Loading...</div>}>
